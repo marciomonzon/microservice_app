6d1b6a6250e7bb4bf8bb2f8f6b8d2e049bd3630b
marciomonzon <marcio.pcmonzon@gmail.com>
2023-08-05 17:56:59 -0300

    feat: platform controller

--- PlatformService/Controllers/PlatformsController.cs
+++ PlatformService/Controllers/PlatformsController.cs
@@ -1,4 +1,8 @@
+using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
+using PlatformService.Data;
+using PlatformService.Dtos;
+using PlatformService.Models;
 
 namespace PlatformService.Controllers
 {
@@ -6,6 +10,46 @@ namespace PlatformService.Controllers
     [Route("api/[controller]")]
     public class PlatformsController : ControllerBase
     {
-                
+        private readonly IPlatformRepository _repository;
+        private readonly IMapper _mapper;
+
+        public PlatformsController(IPlatformRepository repository, IMapper mapper)
+        {
+            _repository = repository;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<PlatformReadDto>> GetPlatforms()
+        {
+            Console.WriteLine("--> Getting Platforms...");
+
+            var platformsItems = _repository.GetAllPlatforms();
+
+            return Ok(_mapper.Map<IEnumerable<PlatformReadDto>>(platformsItems));
+        }
+
+        [HttpGet("{id}", Name = "GetPlatformById")]
+        public ActionResult<PlatformReadDto> GetPlatformById(int id)
+        {
+            var platformItem = _repository.GetPlatformById(id);
+
+            return platformItem != null ? 
+                   Ok(_mapper.Map<PlatformReadDto>(platformItem)) 
+                   : NotFound();
+        }
+
+        [HttpPost]
+        public ActionResult<PlatformReadDto> CreatePlatform(PlatformCreateDto platform)
+        {
+            var model = _mapper.Map<Platform>(platform);
+
+            _repository.CreatePlatform(model);
+            _repository.SaveChanges();
+
+            var platformDto = _mapper.Map<PlatformReadDto>(model);
+
+            return CreatedAtRoute(nameof(GetPlatformById),  new { Id = platformDto.Id }, platformDto);
+        }
     }
 }
